b0373eace3a98676c3a499ebdf39ea2f400b2f98
Hakan Berke İÇELLİOĞLU <hakanicellioglu@gmail.com>
2024-08-30 09:51:05 +0300

    Enhanced comments in `TodoItem` component to clarify functionality.

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -1,19 +1,27 @@
 import React from 'react';
 
+// TodoItem bileşeni, bireysel bir görev öğesini temsil eder
 const TodoItem = ({ todo, deleteTodo, toggleTodo }) => {
     return (
         <li>
+            {
+                /* Görev tamamlanma durumunu gösteren onay kutusu */
+            }
             <input
                 type="checkbox"
-                checked={todo.completed}
-                onChange={() => toggleTodo(todo._id, !todo.completed)}
+                checked={todo.completed} // Onay kutusunun işaretli olup olmadığını belirle
+                onChange={() => toggleTodo(todo._id, !todo.completed)} // Onay kutusu değiştiğinde görev tamamlanma durumunu değiştir
             />
+            {
+                /* Görev başlığını ve tamamlanmışsa ilgili sınıfı uygulayan <span> etiketi */
+            }
             <span className={`todo-text ${todo.completed ? 'completed' : ''}`}>
-                {todo.title}
+                {todo.title} {/* Görev başlığını göster */}
             </span>
+            {/* Görevi silmek için bir düğme */}
             <button onClick={() => deleteTodo(todo._id)}>Sil</button>
         </li>
     );
 };
 
-export default TodoItem;
+export default TodoItem; // Bileşeni dışa aktar
